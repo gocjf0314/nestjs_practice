795b1a817dc8f5844bff37665410cbf530973c99
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-03-28 07:48:57 +0900

    Setting items of user module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,22 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const {name, email} = createUserDto;
+    return `새로운 유저 생성: 이름(${name}}) / 이메일(${email})`;
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
+  findOneWithHeader(id: number) {
+    return 'Found One user with header';
+  }
+
   update(id: number, updateUserDto: UpdateUserDto) {
+    if (+id < 1) {
+      throw new BadRequestException('Id는 0보다 작은 값이여야 합니다.');
+    }
+    
     return `This action updates a #${id} user`;
   }
 

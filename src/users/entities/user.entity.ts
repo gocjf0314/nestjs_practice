d81f1068f81a633c741f976f484c24f76a23d29e
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-03-28 07:48:15 +0900

    Create User Schema

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { UserType } from "../users.type";
+
+export class User {
+    uid: string;
+    name: string;
+    email: string;
+    type: UserType;
+
+    constructor({uid, name, email, type}) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.type = type;
+    };
+}
---
+++ src/users/users.type.ts
@@ -0,0 +1,5 @@
+export enum UserType {
+    NORMAL = "NORMAL",
+    SPECIAL = "SPECIAL",
+    ROYAL = "ROYAL",
+}

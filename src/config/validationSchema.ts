4bb0f6ca442e301ac80bbcc7ddf587cf0daf8843
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-04-04 02:33:35 +0900

    Cleaned up code

--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -1,8 +1,8 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const ValidationSchema = Joi.object({
-    EMAIL_SERVICE: Joi.string().required(),
-    EMAIL_AUTH_USER: Joi.string().required(),
-    EMAIL_AUTH_PASSWORD: Joi.string().required(),
-    EMAIL_BASE_URL: Joi.string().required().uri(),
+  EMAIL_SERVICE: Joi.string().required(),
+  EMAIL_AUTH_USER: Joi.string().required(),
+  EMAIL_AUTH_PASSWORD: Joi.string().required(),
+  EMAIL_BASE_URL: Joi.string().required().uri(),
 });
